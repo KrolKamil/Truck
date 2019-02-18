f228b638d3d621de7124c67e398d3fa2c4111eb6
KrolKamil <kamilkrol1997@wp.pl>
2019-02-18 01:27:21 +0100

    working on post-addToDB

--- classes/Transit.php
+++ classes/Transit.php
@@ -17,9 +17,27 @@ class Transit
 
         while ($row = $stm->fetch())
             $data[] = $row;
-
-
         return $data;
     }
+
+    public function addTransit($sourceAddress, $destinationAddress, $price, $date)
+    {
+        $sql = "INSERT INTO transit ('id', 'source_address', 'destination_address', 'price', 'date')
+                VALUES ('', :source_address, :destination_address, :destination_address, :price, :date)";
+
+        $stmt = $this->db->prepare($sql);
+
+        $result = $stmt->execute([
+            "source_address" => $sourceAddress,
+            "destination_address" => $destinationAddress,
+            "price" => $price,
+            "date" => $date
+        ]);
+        if(!$result) {
+            throw new Exception("could not save record");
+        }
+
+    }
+
 
 }
--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,21 @@ use Slim\Http\Response;
 
 // Routes
 
+/*
+ *
+ *  $data = $request->getParsedBody();
+    $task_data = [];
+    $task_data['name'] = filter_var($data['name'], FILTER_SANITIZE_STRING);
+    if($task_data['name'] !="")
+    {
+        $task = new TaskEntity($task_data);
+        $task_mapper = new TaskMapper($this->db);
+        $task_mapper->addTask($task);
+    }
+    $response = $response->withRedirect($this->router->pathFor('task-show'));
+    return $response;
+ */
+
 $app->get('/transits', function(Request $request, Response $response){
     $this->logger->info('fetch all data');
     $transit = new Transit($this->db);
@@ -14,6 +29,11 @@ $app->get('/transits', function(Request $request, Response $response){
 
     return $response;
 })->setName('transits');
+
+$app->post('/transits', function (Request $request, Response $response)
+{
+    //$data = $request->getParsedBody();
+});
 
 
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
